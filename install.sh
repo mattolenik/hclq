748edaf5f8bf17a8d321c18685879b281ad966b0
Matt Olenik <mattolenik@curalate.com>
2017-10-27 14:28:00 -0700

    Remove jq dependency from install script

--- install.sh
+++ install.sh
@@ -6,12 +6,6 @@ set -e
 destination=${1:-"/usr/local/bin"}
 [ ! -d "$destination" ] && printf "Install directory '%s' does not exist\n", "$destination" && exit 1
 
-if ! command -v jq > /dev/null 2>&1; then
-  printf "jq is required for this install script"
-  [ "$(uname)" = "Darwin" ] && printf ", it can be installed with 'brew install jq\n'"
-  exit 1
-fi
-
 if command -v hclq > /dev/null 2>&1; then
   msg="Upgrading hclq..."
   ver="$(hclq --version)"
@@ -20,11 +14,11 @@ else
 fi
 
 latest="$(curl -s https://api.github.com/repos/mattolenik/hclq/releases/latest)"
-tag="$(printf "$latest" | jq -r '.tag_name')"
+tag="$(printf "$latest" | grep tag_name | awk -F'"' '{print $4}')"
 [ "$tag" = "$ver" ] && printf "hclq is already at the latest version\n" && exit 0
 printf "%s\n" "$msg"
 
-hclq_url=$(curl -s https://api.github.com/repos/mattolenik/hclq/releases/latest | jq -r '.assets[] | .browser_download_url' | grep -i "$(uname)")
+hclq_url=$(curl -s https://api.github.com/repos/mattolenik/hclq/releases/latest | grep -i "browser_download_url.*$(uname)" | awk -F'"' '{print $4}')
 curl --progress-bar -JLo "$destination/hclq" "$hclq_url" && chmod +x "$destination/hclq"
 
 printf "hclq now at version %s\n" "$(hclq --version)"

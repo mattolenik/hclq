a7164c85538b6c723d7b53e018250b80638db51f
Matt Olenik <mattolenik@curalate.com>
2017-10-26 17:27:21 -0700

    Add append, prepend, replace, refactor

--- main.go
+++ main.go
@@ -30,10 +30,16 @@ func main() {
 HCL Query/Edit Tool
 
 Usage:
-  hclq get [options] <nodePath> <file>
-  hclq get [options] <nodePath> [-]
-  hclq set [options] <nodePath> <value> <file>
-  hclq set [options] <nodePath> <value> [-]
+  hclq get     [options] <node> <file>
+  hclq get     [options] <node> [-]
+  hclq set     [options] <node> <value> <file>
+  hclq set     [options] <node> <value> [-]
+  hclq append  [options] <node> <value> <file>
+  hclq append  [options] <node> <value> [-]
+  hclq prepend [options] <node> <value> <file>
+  hclq prepend [options] <node> <value> [-]
+  hclq replace [options] <node> <value> <newValue> <file>
+  hclq replace [options] <node> <value> <newValue> [-]
 
   hclq --help
   hclq --version
@@ -49,13 +55,62 @@ Options:
 	queryNodes := query.Parse(arguments["<nodePath>"].(string))
 
 	raw := arguments["--raw"].(bool)
+	inPlace := arguments["--in-place"].(bool)
+	file, _ := arguments["<file>"].(string)
+	value, valueOk := arguments["<value>"].(string)
+	newValue, newValueOk := arguments["<newValue>"].(string)
 
-	var err error
-	if arguments["get"].(bool) {
-		err = get(arguments, queryNodes, raw)
-	} else if arguments["set"].(bool) {
-		err = set(arguments, queryNodes)
-	}
+	err := func() error {
+		if arguments["get"].(bool) {
+			return get(arguments, queryNodes, raw)
+
+		} else if arguments["set"].(bool) {
+			if !valueOk {
+				return errors.New("<value> required for set command")
+			}
+			return set(
+				file,
+				queryNodes,
+				func(s string) string { return value },
+				inPlace)
+
+		} else if arguments["append"].(bool) {
+			if !valueOk {
+				return errors.New("<value> required for append command")
+			}
+			return set(
+				file,
+				queryNodes,
+				func(s string) string { return s + value },
+				inPlace)
+
+		} else if arguments["prepend"].(bool) {
+			if !valueOk {
+				return errors.New("<value> required for prepend command")
+			}
+			return set(
+				file,
+				queryNodes,
+				func(s string) string { return value + s },
+				inPlace)
+
+		} else if arguments["replace"].(bool) {
+			if !valueOk {
+				return errors.New("<value> required for replace command")
+			}
+			if !newValueOk {
+				return errors.New("<newValue> required for replace command")
+			}
+			return set(
+				file,
+				queryNodes,
+				func(s string) string {
+					return strings.Replace(s, value, newValue, -1)
+				},
+				inPlace)
+		}
+		return errors.New("unknown command")
+	}()
 	if err != nil && !arguments["--quiet"].(bool) {
 		getErrorOutput(err, raw)
 		os.Exit(1)
@@ -164,13 +219,11 @@ func getImpl(node ast.Node, query []query.Node, queryIdx int) (interface{}, erro
 	return nil, errors.New("unhandled case")
 }
 
-func set(arguments map[string]interface{}, query []query.Node) error {
+func set(fileName string, query []query.Node, value func(original string) string, inPlace bool) error {
 	var hcl []byte
 	var err error
-	var fileName string
-	var ok bool
 
-	if fileName, ok = arguments["<file>"].(string); ok {
+	if fileName != "" {
 		hcl, err = ioutil.ReadFile(fileName)
 		if err != nil {
 			return err
@@ -187,12 +240,12 @@ func set(arguments map[string]interface{}, query []query.Node) error {
 		return err
 	}
 
-	err = setImpl(node.Node, query, arguments["<value>"].(string), 0)
+	err = setImpl(node.Node, query, value, 0)
 	if err != nil {
 		return err
 	}
 
-	if arguments["-i"].(bool) {
+	if inPlace {
 		file, err := os.Create(fileName)
 		if err != nil {
 			return err
@@ -205,7 +258,7 @@ func set(arguments map[string]interface{}, query []query.Node) error {
 	return nil
 }
 
-func setImpl(node ast.Node, query []query.Node, value string, queryIdx int) error {
+func setImpl(node ast.Node, query []query.Node, value func(original string) string, queryIdx int) error {
 	if objList, ok := node.(*ast.ObjectList); ok {
 		for _, obj := range objList.Items {
 			err := setImpl(obj, query, value, queryIdx)
@@ -233,12 +286,12 @@ func setImpl(node ast.Node, query []query.Node, value string, queryIdx int) erro
 		return setImpl(objItem.Val, query, value, queryIdx)
 	}
 	if literal, ok := node.(*ast.LiteralType); ok {
-		literal.Token.Text = value
+		literal.Token.Text = value(literal.Token.Text)
 		return nil
 	}
 	if list, ok := node.(*ast.ListType); ok {
 		// HCL JSON parser needs a top level object
-		jsonValue := fmt.Sprintf(`{"root": %s}`, value)
+		jsonValue := fmt.Sprintf(`{"root": %s}`, )
 		tree, err := jsonParser.Parse([]byte(jsonValue))
 		if err != nil {
 			return err

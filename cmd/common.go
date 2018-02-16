fe24b750a5a727ffabfce50841f753576396737a
Matt Olenik <olenikm@gmail.com>
2018-02-16 10:56:54 -0800

    Implement raw output properly

--- cmd/common.go
+++ cmd/common.go
@@ -3,11 +3,22 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
 func getOutput(obj interface{}, raw bool) (string, error) {
 	if raw {
-		return fmt.Sprintf("%+v", obj), nil
+		result := ""
+		arr, isArray := obj.([]interface{})
+		if isArray {
+			for _, item := range arr {
+				result += fmt.Sprintf("%+v", item) + " "
+			}
+			result = strings.TrimRight(result, " ")
+			result += "\n"
+			return result, nil
+		}
+		return fmt.Sprintf("%+v\n", obj), nil
 	}
 	jsonBody, err := json.Marshal(obj)
 	if err != nil {

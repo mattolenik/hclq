2432f7d44c8f530d45ff07d8b6c176d997dbe809
Matt Olenik <olenikm@gmail.com>
2019-02-02 15:13:35 -0800

    Proper error output

--- cmd/get.go
+++ cmd/get.go
@@ -13,42 +13,49 @@ import (
 var GetCmd = &cobra.Command{
 	Use:   "get <query>",
 	Short: "retrieve values matching <query>",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		qry, _ := query.Parse(args[0])
-		reader := os.Stdin
-		if val := inFile; val != "" {
-			var err error
-			reader, err = os.Open(val)
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		e := func() error {
+			qry, _ := query.Parse(args[0])
+			reader := os.Stdin
+			if val := inFile; val != "" {
+				var err error
+				reader, err = os.Open(val)
+				if err != nil {
+					return err
+				}
+			}
+			raw := cmd.Flag("raw").Value.String() == "true"
+			resultPairs, isList, _, err := query.HCL(reader, qry)
 			if err != nil {
 				return err
 			}
-		}
-		raw := cmd.Flag("raw").Value.String() == "true"
-		resultPairs, isList, _, err := query.HCL(reader, qry)
-		if err != nil {
-			return err
-		}
-		results := []interface{}{}
-		for _, pair := range resultPairs {
-			results = append(results, pair.Value)
-		}
-		// The return type can be a list if: the queried object IS a list, or if the query matched multiple single items
-		// So, return now if it's not a list and there is only one query result
-		if !isList && len(results) == 1 {
-			output, err := getOutput(results[0], raw)
+			results := []interface{}{}
+			for _, pair := range resultPairs {
+				results = append(results, pair.Value)
+			}
+			// The return type can be a list if: the queried object IS a list, or if the query matched multiple single items
+			// So, return now if it's not a list and there is only one query result
+			if !isList && len(results) == 1 {
+				output, err := getOutput(results[0], raw)
+				if err != nil {
+					return err
+				}
+				fmt.Print(output)
+				return err
+			}
+			output, err := getOutput(results, raw)
 			if err != nil {
 				return err
 			}
+			output = strings.Trim(output, " \n")
 			fmt.Print(output)
-			return err
+			return nil
+		}()
+		if e != nil {
+			fmt.Fprintln(os.Stderr, e.Error())
+			os.Exit(1)
 		}
-		output, err := getOutput(results, raw)
-		if err != nil {
-			return err
-		}
-		output = strings.Trim(output, " \n")
-		fmt.Print(output)
-		return nil
 	},
 }
 

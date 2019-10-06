6ab0a6506433fb2ae3572413c9cc2a68f51fc606
Matt Olenik <olenikm@gmail.com>
2019-10-06 15:06:21 -0700

    Handle multiline and unknown chars

--- main.go
+++ main.go
@@ -12,23 +12,31 @@ import (
 
 func main() {
 	lxr := lexer.Must(ebnf.New(`
-		Dot = "." .
-		Regex = "/" { "\\/" | anyregex } "/" .
-		Ident = { "\\." | anyident } | { anyident } .
-		alpha = "a"…"z" | "A"…"Z" .
-		any = "\u0000"…"\uffff" .
-		anyident = "\u0000"…"\uffff"-"." .
-		anyregex = "\u0000"…"\uffff"-"/" .
+		FilterSeparator     = "|" .
+		QuerySeparator      = "." | Whitespace "." .
+		Regex               = "/" { "\\/" | anyregex } "/" .
+		Ident               = { "\\." | anyident } | { anyident } .
+		Whitespace          = "\r\n" | "\n" | "\r" | " " | "\t" .
+
+		alpha           = "a"…"z" | "A"…"Z" .
+		invalid         = "\u0000"…"\u0040" | "\u007B"…"\u00BF" .
+		any             = "\u0041"…"\u007A" | "\u00C0"…"\uFFFF" .
+		anyident        = "\u0041"…"\u007A" - "." | "\u00C0"…"\uFFFF" .
+		anyregex        = "\u0041"…"\u007A" - "/" | "\u00C0"…"\uFFFF" .
 	`))
 
 	parser, err := participle.Build(
 		&Query{},
 		participle.Lexer(lxr),
+		participle.Elide("Whitespace", "invalid"),
 	)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	q := `.abc.def./abc\/sadf/.xy\.z`
+	q := `.abc
+.def
+./abc\/sadf/.xy\.z`
 	qAST := &Query{}
 	err = parser.ParseString(q, qAST)
 	if err != nil {
@@ -39,7 +47,7 @@ func main() {
 }
 
 type Query struct {
-	Crumbs []*Crumb `( Dot @@ )+`
+	Crumbs []*Crumb `( QuerySeparator @@ )+`
 }
 
 type Crumb struct {
